1a3019463d08792012be382f27f245b5094bbc95
caiguojing <1010784344@qq.com>
2021-06-24 11:05:35 +0800

    项目路由整理

---
+++ dn_django/django_code01/django_code/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+from django.http import HttpResponse
+
+
+def page_view(request, id):
+    str = u'这是编号为%s的网页' % id
+    return HttpResponse(str)
+
+
+def index_view(request):
+    str = u'这是我的首页'
+    return HttpResponse(str)
+
+
+def cal_view(request, x, op, y):
+    html = 'x:%s, op:%s, y:%s' % (x, op, y)
+    return HttpResponse(html)
+
+
+
+if __name__ == '__main__':
+    print(1)
